061adcf34d5eff0990c08a1e36d22f60cf40bde7
Asif.Ahmed <asif.ahmed@atco.com>
2024-10-09 14:02:17 -0400

    removed the debug label

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Solid Soft Test',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
